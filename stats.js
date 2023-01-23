9a4bbd7662afebce8e6bb11233713afc693e7111
renato.mello <renato.mello@globalhitss.com.br>
2023-01-23 11:46:42 -0300

    commit final

--- stats.js
+++ stats.js
@@ -1,1 +1,18 @@
 const os = require("os")
+
+setInterval(() => {
+	const { freemem, totalmem } = os
+
+	const total = parseInt(totalmem() / 1024 / 1024)
+	const free = parseInt(freemem() / 1204 / 1024)
+	const percentage = parseInt((free / total) * 100)
+
+	const stats = {
+		total: `${total} MB`,
+		free: `${free} MB`,
+		percentage: `${percentage}%`,
+	}
+	console.clear()
+	console.log("==== PC Stats =====")
+	console.table(stats)
+}, 1000)
